f15778702fab0081deb884331aacc07e3f19dd7c
Tatenda Kabanda <dennis99tatenda@gmail.com>
2025-12-06 23:05:15 +0300

    Used the Azure AI Agent Service SDK and the A2A Python SDK to create a remote multi-agent solution. Created a discoverable A2A-compatible agent and set up a routing agent to access the agent’s skills. Implemented an agent executor to process incoming A2A messages and manage tasks.

--- Labfiles/06-build-remote-agents-with-a2a/python/routing_agent/agent.py
+++ Labfiles/06-build-remote-agents-with-a2a/python/routing_agent/agent.py
@@ -112,7 +112,7 @@ class RoutingAgent:
             raise ValueError(f'Agent {agent_name} not found')
         
         # Retrieve the remote agent's A2A client using the agent name 
-        
+        client = self.remote_agent_connections[agent_name]
 
         if not client:
             raise ValueError(f'Client not available for {agent_name}')
@@ -120,13 +120,19 @@ class RoutingAgent:
         message_id = str(uuid.uuid4())
 
         # Construct the payload to send to the remote agent
-        
+        payload: dict[str, Any] = {
+            'message': {
+                'role': 'user',
+                'parts': [{'kind': 'text', 'text': task}],
+                'message_id': message_id,
+            },
+        }
         
         # Wrap the payload in a SendMessageRequest object
-        
+        message_request = SendMessageRequest(id=message_id, params=MessageSendParams.model_validate(payload))
 
         # Send the message to the remote agent client and await the response
-        
+        send_response: SendMessageResponse = await client.send_message(message_request=message_request)
         
         if not isinstance(send_response.root, SendMessageSuccessResponse):
             print('received non-success response. Aborting get task ')
@@ -247,8 +253,8 @@ async def _get_initialized_routing_agent_sync() -> RoutingAgent:
     async def _async_main() -> RoutingAgent:
         routing_agent_instance = await RoutingAgent.create(
             remote_agent_addresses=[
-                f"http://{os.environ["SERVER_URL"]}:{os.environ["TITLE_AGENT_PORT"]}",
-                f"http://{os.environ["SERVER_URL"]}:{os.environ["OUTLINE_AGENT_PORT"]}",
+                f"http://{os.environ['SERVER_URL']}:{os.environ['TITLE_AGENT_PORT']}",
+                f"http://{os.environ['SERVER_URL']}:{os.environ['OUTLINE_AGENT_PORT']}",
             ]
         )
         # Create the Azure AI agent
